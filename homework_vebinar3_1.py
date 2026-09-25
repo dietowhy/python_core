39e49e6535b65baf653d5ce34cdaa17348deafa4
dietowhy <pshenichnyy.i@list.ru>
2026-09-25 16:30:37 +0300

    some fixes

--- homework_vebinar3_1.py
+++ homework_vebinar3_1.py
@@ -1,6 +1,8 @@
 # Задание 1
 for i in range(1, 31):
-    if i % 3 == 0:
+    if i % 3 == 0 and i % 5 == 0:
+        print("BugTest")
+    elif i % 3 == 0:
         print("Bug")
     elif i % 5 == 0:
         print("Test")
--- homework_vebinar3_2.py
+++ homework_vebinar3_2.py
@@ -6,7 +6,4 @@ for i in range(3):
         print("Logged in successfully.")
         break
     else:
-        print("Access denied!")
-
-
-
+        print("Access blocked!")
